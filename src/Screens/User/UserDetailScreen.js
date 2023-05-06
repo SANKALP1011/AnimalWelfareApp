245cf7d344a4b1dddbbf388ef5707e02a3ff6766
SANKALP PANDEY <84143581+SANKALP1011@users.noreply.github.com>
2023-05-06 20:41:17 +0530

    done with the user details screen as of now

--- src/Screens/User/UserDetailScreen.js
+++ src/Screens/User/UserDetailScreen.js
@@ -1,14 +1,15 @@
 import { useEffect } from "react";
-import { View, StyleSheet, Text, Dimensions } from "react-native";
+import { View, StyleSheet, Text, Dimensions, Pressable } from "react-native";
 import { AppAuthContext } from "../../Context/AuthProvider";
 import { useContext } from "react";
 import Profile from "../../Assets/Profile.png";
 import ProfilePic from "../../Components/ProfilePic";
 import { BigCard } from "../../Components/BigCard";
-import { FontAwesome5, Foundation } from "@expo/vector-icons";
+import { FontAwesome5, Foundation, FontAwesome } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const appWidth = Dimensions.get("screen").width;
-export const UserDetailScreen = ({ navigator }) => {
+export const UserDetailScreen = ({ navigation }) => {
   const { user, updateUser } = useContext(AppAuthContext);
   var animalReportedData = [];
   animalReportedData.push(user.animalReported);
@@ -47,7 +48,34 @@ export const UserDetailScreen = ({ navigator }) => {
               <Text style={styles.iconText}>0</Text>
             </View>
           </View>
-          <View></View>
+          <View style={styles.detailsBox}>
+            <View style={styles.detailsContainer}>
+              <FontAwesome name="phone" size={40} color="black" />
+              {user.Number ? (
+                <Text style={styles.detailsText}>{user.Number}</Text>
+              ) : (
+                <Text style={styles.detailsText}>No Number is there...</Text>
+              )}
+            </View>
+            <View style={styles.detailsContainer}>
+              <MaterialIcons name="pets" size={40} color="black" />
+              {user.PetDetails && user.PetDetails.Petname ? (
+                <Text style={styles.detailsText}>
+                  {user.PetDetails.Petname}
+                </Text>
+              ) : (
+                <Text style={styles.detailsText}>No pet exists</Text>
+              )}
+            </View>
+            <Pressable
+              style={styles.button}
+              onPress={() => {
+                navigation.navigate("UserHome");
+              }}
+            >
+              <Text style={styles.text}>Home</Text>
+            </Pressable>
+          </View>
         </View>
       ) : (
         <View>
@@ -113,6 +141,48 @@ const styles = StyleSheet.create({
   iconText: {
     fontWeight: "bold",
     marginTop: 5,
+  },
+  detailsBox: {
+    backgroundColor: "#9F73AB",
+    height: 500,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+  },
+  detailsContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 80,
+    marginTop: 50,
+  },
+  detailsText: {
+    fontSize: 15,
+    fontWeight: "bold",
+    marginLeft: 20,
+  },
+  button: {
+    width: 100,
+    height: 40,
+    marginTop: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 4,
+    backgroundColor: "black",
+    borderRadius: 15,
+    marginLeft: 140,
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
   },
 });
 export default UserDetailScreen;
