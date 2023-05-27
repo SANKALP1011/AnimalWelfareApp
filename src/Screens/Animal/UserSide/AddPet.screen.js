e5837e97d6a2d9d6981ad8013e6f38ddcfa25bb5
SANKALP PANDEY <84143581+SANKALP1011@users.noreply.github.com>
2023-05-27 15:49:12 +0530

    working on the add pet s reen

--- src/Screens/Animal/UserSide/AddPet.screen.js
+++ src/Screens/Animal/UserSide/AddPet.screen.js
@@ -1,10 +1,42 @@
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import RadioButton from "../../../Components/RadioButton";
 
 export const AddPet = ({ navigation }) => {
+  const [value, selectedValue] = useState("");
+
+  const data = ["Dog", "Cat", "Other"];
+  const handleChoice = (value) => {
+    selectedValue(value);
+    console.log(value);
+  };
   return (
-    <View>
-      <Text>ADD PET SCREEN</Text>
+    <View style={styles.container}>
+      <View style={styles.radioButton}>
+        <RadioButton
+          data={data}
+          styles={styles.radioButtonContainer}
+          onSelect={handleChoice}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  radioButton: {
+    flexDirection: "row",
+  },
+  radioButtonContainer: {
+    width: 100,
+    height: 50,
+    backgroundColor: "blue",
+    marginBottom: 20,
+  },
+});
+
 export default AddPet;
