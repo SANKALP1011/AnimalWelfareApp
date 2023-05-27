2e8e8f472817acf5c489b391e79a3af593803da7
SANKALP PANDEY <84143581+SANKALP1011@users.noreply.github.com>
2023-05-28 01:40:11 +0530

    working on rhe ui

--- src/Components/RadioButton.js
+++ src/Components/RadioButton.js
@@ -1,28 +1,37 @@
 import { View, Text, StyleSheet, Pressable } from "react-native";
 import react, { useState } from "react";
 
-export const RadioButton = ({ data, styles, onSelect }) => {
+export const RadioButton = ({ data, customStyles, onSelect }) => {
   const [selected, setSelected] = useState("");
   const handleSelectedValue = (value) => {
     setSelected(value);
     onSelect(value);
   };
   return (
-    <View>
+    <View style={styles.container}>
       {data.map((item, index) => {
         return (
           <Pressable
-            style={styles}
+            style={customStyles}
             key={index}
             onPress={() => {
               handleSelectedValue(item);
             }}
           >
-            <Text>{item}</Text>
+            <Text style={styles.textStyle}>{item}</Text>
           </Pressable>
         );
       })}
     </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+  },
+  textStyle: {
+    fontSize: 17,
+    fontWeight: "bold",
+  },
+});
 export default RadioButton;
